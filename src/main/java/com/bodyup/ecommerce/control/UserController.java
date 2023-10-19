334f5f29851f5ab57c8d199a20676ebf1902c2c1
Rennan Bacili <rennanbaccili@gmail.com>
2023-10-19 07:53:40 -0300

    Cret User Table

---
+++ src/main/java/com/bodyup/ecommerce/control/UserController.java
@@ -0,0 +1,21 @@
+package com.bodyup.ecommerce.control;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bodyup.ecommerce.model.User;
+
+//controlador da camada rest
+@RestController
+@RequestMapping(value= "/users")
+public class UserController {
+
+	//mapeado no padrão rest
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria","123","email","123");
+		return ResponseEntity.ok().body(u);
+	}
+}
---
+++ src/main/java/com/bodyup/ecommerce/model/User.java
@@ -0,0 +1,89 @@
+package com.bodyup.ecommerce.model;
+
+import java.io.Serializable;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//implementa classe serializable
+
+@Entity
+@Table(name="tab_user")
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	//defino a chave primaria, com valor sera gerado automaticamente
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String cpf;
+	private String email;
+	private String password;
+	
+	
+	
+	//construtores
+	public User() {
+	}
+
+	public User(Long id, String name, String cpf, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.email = email;
+		this.password = password;
+	}
+
+	//getters setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+	
+	
+	
+}
