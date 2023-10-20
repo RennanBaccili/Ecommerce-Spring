b9b65784abc8e347f52f9251430dddb785b7cfe5
Rennan Bacili <rennanbaccili@gmail.com>
2023-10-20 16:59:47 -0300

    tratando exceptions delete e put do user

--- src/main/java/com/bodyup/ecommerce/services/UserService.java
+++ src/main/java/com/bodyup/ecommerce/services/UserService.java
@@ -5,11 +5,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.bodyup.ecommerce.model.User;
 import com.bodyup.ecommerce.repositories.UserRepository;
+import com.bodyup.ecommerce.services.exceptions.DatabaseException;
 import com.bodyup.ecommerce.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -30,14 +35,26 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void deleteById(Long id) {
-		repository.deleteById(id);
+		try{
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) { // exception da camada de cerviço
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
 		// getReference observa o id passado... o metodo updateData defini quais atributos serão alterados
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 	// datas que serão atualizadas
 	public void updateData(User entity, User obj) {
